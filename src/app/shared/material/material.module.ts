167917b6fcf584cf4d88e8ee70437b02cce3f0b4
asem1789 <as.1789@hotmail.com>
2021-10-27 14:01:30 +0300

    registry suggestion icon into material icon

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { SuggestionIcon } from 'src/assets/icons/svgs';
 
 const Mat_Module = [MatIconModule];
+
+const CustomIcons = [{ name: 'suggestion_icon', svg: SuggestionIcon }];
 
 @NgModule({
   declarations: [],
   imports: [...Mat_Module],
   exports: [...Mat_Module],
 })
-export class MaterialModule {}
+export class MaterialModule {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitzer: DomSanitizer
+  ) {
+    CustomIcons.forEach((el) => {
+      this.matIconRegistry.addSvgIconLiteral(
+        el.name,
+        this.domSanitzer.bypassSecurityTrustHtml(el.svg)
+      );
+    });
+  }
+}
